e0161a2fb2b3b57a3f915886549b5125b6161c46
whiteadder <whiteadder@gmail.com>
2019-12-31 01:26:18 +0000

    add sass support

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import HomePage from "./homepage.component";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
