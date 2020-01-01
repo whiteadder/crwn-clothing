22b66ce632a714ce910e0029e70de7f4ed8f1859
white <whiteadder@gmail.com>
2020-01-01 12:31:30 +0000

    firebase:signin signout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
@@ -7,11 +7,22 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
+import { auth } from "./firebase/firebase.utils";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState("");
+
+  useEffect(() => {
+    const unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      setCurrentUser(user);
+      console.log(user);
+    });
+    return () => unsubscribeFromAuth();
+  }, []);
+
   return (
-    <div className="App">
-      <Header />
+    <div>
+      <Header currentUser={currentUser} />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
